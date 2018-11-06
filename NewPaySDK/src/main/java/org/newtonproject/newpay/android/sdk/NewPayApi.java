47943ff9c8d1023c789afd2bbb3ac038c7e051a1
weixuefeng <weixuefeng@lubangame.com>
2018-11-06 16:48:07 +0800

    add pay for third app

--- NewPaySDK/src/main/java/org/newtonproject/newpay/android/sdk/NewPayApi.java
+++ NewPaySDK/src/main/java/org/newtonproject/newpay/android/sdk/NewPayApi.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
-import android.os.Bundle;
 import android.widget.Toast;
 
 import org.web3j.crypto.ECKeyPair;
@@ -14,6 +13,7 @@ import org.web3j.crypto.Sign;
 import org.web3j.utils.Numeric;
 
 import java.lang.ref.WeakReference;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Random;
 
@@ -23,7 +23,8 @@ public class NewPayApi {
     private static String privateKey;
     private static String appId;
 
-    public static final int REQUEST_CODE_NEWPAY = 3000;
+    public static final int REQUEST_CODE_NEWPAY = 3001;
+    public static final int REQUEST_CODE_NEWPAY_PAY = 3002;
 
     private static WeakReference<Activity> mCurrentActivity;
 
@@ -33,46 +34,9 @@ public class NewPayApi {
         mApplication = context;
         privateKey = appKey;
         appId = appid;
-        mApplication.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
-            @Override
-            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-
-            }
-
-            @Override
-            public void onActivityStarted(Activity activity) {
-
-            }
-
-            @Override
-            public void onActivityResumed(Activity activity) {
-                mCurrentActivity = new WeakReference<>(activity);
-            }
-
-            @Override
-            public void onActivityPaused(Activity activity) {
-
-            }
-
-            @Override
-            public void onActivityStopped(Activity activity) {
-
-            }
-
-            @Override
-            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-
-            }
-
-            @Override
-            public void onActivityDestroyed(Activity activity) {
-                mCurrentActivity.clear();
-                mCurrentActivity = null;
-            }
-        });
     }
 
-    public static void requestProfileFromNewPay() {
+    public static void requestProfileFromNewPay(Activity activity) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("newpay://org.newtonproject.newpay.android.authorize"));
         String message = getMessage();
         Sign.SignatureData sig = getSignMessage(message, privateKey);
@@ -85,9 +49,27 @@ public class NewPayApi {
         boolean isIntentSafe = activities.size() > 0;
         // Start an activity if it's safe
         if (isIntentSafe) {
-            mCurrentActivity.get().startActivityForResult(intent, REQUEST_CODE_NEWPAY);
+            activity.startActivityForResult(intent, REQUEST_CODE_NEWPAY);
         } else{
-            Toast.makeText(mCurrentActivity.get(), R.string.no_newpay_application, Toast.LENGTH_SHORT).show();
+            Toast.makeText(activity, R.string.no_newpay_application, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static void requestPayForThirdApp(Activity activity, String address, BigInteger account){
+        String unitStr = "NEW";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("newpay://org.newtonproject.newpay.android.pay"));
+        intent.putExtra("SYMBOL", unitStr);
+        intent.putExtra("ADDRESS", address);
+        intent.putExtra("AMOUNT", account.toString(10));
+        intent.putExtra("REQUEST_PAY_SOURCE", activity.getPackageName());
+        PackageManager packageManager = mApplication.getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(intent, 0);
+        boolean isIntentSafe = activities.size() > 0;
+        // Start an activity if it's safe
+        if (isIntentSafe) {
+            activity.startActivityForResult(intent, REQUEST_CODE_NEWPAY_PAY);
+        } else{
+            Toast.makeText(activity, R.string.no_newpay_application, Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/feng/newmalldemo/MainActivity.java
+++ app/src/main/java/com/feng/newmalldemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,6 +19,8 @@ import com.google.gson.Gson;
 import org.newtonproject.newpay.android.sdk.NewPayApi;
 import org.newtonproject.newpay.android.sdk.bean.ProfileInfo;
 import org.newtonproject.newpay.android.sdk.bean.SigMessage;
+
+import java.math.BigInteger;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String privateKey = "0xbc6162af5677bc108fc227a1b1178aede933d05979cc5c6154078c2eae068dac";
     private static final String publicKey = "0xa16b27213b5279d1f43c67e0e43272dad5a19376af3182fc24199d06d8f169402b2a4bc1d609d8af3186b929f42ffee0fba00cb569547fffa3c2769abe55ae7a";
     Gson gson = new Gson();
+    private Button request20Bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         initView();
         NewPayApi.init(getApplication(), privateKey, "1");
+
     }
 
     private void initView() {
@@ -48,14 +53,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         cellphoneTextView = findViewById(R.id.cellphoneTextView);
         newidTextView = findViewById(R.id.newidTextView);
         imageView = findViewById(R.id.avatarImageView);
+        request20Bt = findViewById(R.id.request20Bt);
         profileLinearLayout.setOnClickListener(this);
+        request20Bt.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.profileLayout:
-                NewPayApi.requestProfileFromNewPay();
+                NewPayApi.requestProfileFromNewPay(this);
+                break;
+            case R.id.request20Bt:
+                NewPayApi.requestPayForThirdApp(this,"0x920bc30537e3ea976fea09b8a4f025d20e4c674a",BigInteger.valueOf(100));
                 break;
         }
     }
@@ -86,6 +96,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         if(requestCode == NewPayApi.REQUEST_CODE_NEWPAY && resultCode == RESULT_CANCELED) {
             Toast.makeText(this, "user canceled", Toast.LENGTH_LONG).show();
+        }
+        // pay result
+        if(requestCode == NewPayApi.REQUEST_CODE_NEWPAY_PAY && resultCode == RESULT_OK){
+            if(data != null) {
+                String txid = data.getStringExtra("txid");
+                Toast.makeText(this, "txid is:" + txid, Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "error data is null", Toast.LENGTH_SHORT).show();
+            }
+        }
+        if(requestCode == NewPayApi.REQUEST_CODE_NEWPAY_PAY && resultCode == RESULT_CANCELED){
+            Toast.makeText(this, "User canceled", Toast.LENGTH_SHORT).show();
         }
     }
 }
