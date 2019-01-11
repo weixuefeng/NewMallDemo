54233fd9b41c23c595882249e6f3e274852cefa2
weixuefeng <weixuefeng@lubangame.com>
2019-01-11 19:40:03 +0800

    adjust code for comment

--- NewPaySDK/src/main/java/org/newtonproject/newpay/android/sdk/bean/Order.java
+++ NewPaySDK/src/main/java/org/newtonproject/newpay/android/sdk/bean/Order.java
@@ -1,12 +1,5 @@
 package org.newtonproject.newpay.android.sdk.bean;
 
-/**
- * @author weixuefeng@lubangame.com
- * @version $Rev$
- * @time: 2019/1/11--1:31 PM
- * @description
- * @copyright (c) 2018 Newton Foundation. All rights reserved.
- */
 public class Order {
     public String orderNumber;
     public float price;
